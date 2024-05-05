ba499f62183187530529621b8e0aa58817c094b6
D3crvpt3d <st4r.sprayer@gmail.com>
2024-05-05 15:22:08 +0200

    precalculated constants and improved needed precision

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use astro_float::{BigFloat, RoundingMode};
-
+use::std::io::stdin;
 fn main() {
-    let n = 2000000;
-    let p = (n / (n as f32).log2() as usize).max(64);
-    let one = BigFloat::from_i32(1, p);
-    let two = BigFloat::from_i32(2, p);
-    let rm = RoundingMode::Up;
-    let sqrt_5 = BigFloat::from_i32(5, p).sqrt(n,rm);
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).expect("cant read stdin");
 
-    let inner_one = BigFloat::from_i32(1, p).add(&sqrt_5, p, rm).div(&two, p, rm).powi(n, p, rm);
-    let inner_two = BigFloat::from_i32(1, p).sub(&sqrt_5, p, rm).div(&two, p, rm).powi(n, p, rm);
+    let n:usize = buf.trim().parse::<usize>().expect("cant parse");
+    let p: usize = ((n as f64 * (1.62f64).log2()) as usize).max(32); //needed precision is log_2(golden_ratio^n)
+    let one: BigFloat = BigFloat::from_i32(1, p); //precalc one
+    let two: BigFloat = BigFloat::from_i32(2, p); //precalc two
+    let rm: RoundingMode = RoundingMode::Up;
+    let sqrt_5: BigFloat = BigFloat::from_i32(5, p).sqrt(p,rm); //precalc sqrt(5)
+
+    let inner_one: BigFloat = BigFloat::from_i32(1, p).add(&sqrt_5, p, rm).div(&two, p, rm).powi(n, p, rm);
+    let inner_two: BigFloat = BigFloat::from_i32(1, p).sub(&sqrt_5, p, rm).div(&two, p, rm).powi(n, p, rm);
 
     let fib: BigFloat = one.div(&sqrt_5, p, rm).mul(&inner_one.sub(&inner_two, p, rm), p, rm).int();
 
